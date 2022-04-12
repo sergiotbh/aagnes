2d17fc13cc682e47a5bc08641e2102201a5a4260
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-04-12 15:23:09 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Aagnes`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-react-helmet", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
